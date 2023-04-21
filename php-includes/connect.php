ada264532a4513d110b0b63b0055015edc9ae25f
Aime Didier Mugisha <73910734+aimedidierm@users.noreply.github.com>
2023-04-21 17:45:17 +0200

    Update connect.php

--- php-includes/connect.php
+++ php-includes/connect.php
@@ -1,6 +1,6 @@
 <?php
-define('DB_USER', 'aimedidierm');
-define('DB_PASS', 'MUdidier@123');
+define('DB_USER', 'root');
+define('DB_PASS', '');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'student_card');
 $db = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASS);
